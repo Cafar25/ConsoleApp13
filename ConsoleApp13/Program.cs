7de0f707fc77008c5c62e879b6aa1f23da3bd3a4
jafarka <jafarka@CATR5>
2023-03-05 12:39:46 +0400

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        l1:
+            int a;
+            Console.Write("Uzunlugu:");
+            if (!int.TryParse(Console.ReadLine(), out a))
+            {
+                goto l1;
+            }
+            var student = new dynamic[a];
+            for (int i = 0; i < 10; i++)
+            {
+                student[i] = new
+                {
+                    name = Console.ReadLine(),
+                    surname = Console.ReadLine(),
+                    speciality = Console.ReadLine(),
+                    age = int.Parse(Console.ReadLine()),
+                    GroupNo = int.Parse(Console.ReadLine())
+                };
+
+                Console.WriteLine(student[i]);
+            }
+           
+        }
+    }
+}
